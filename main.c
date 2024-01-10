c76fab45c07935a943e1c8fcdfcec924620b52fe
leogeek69 <145578837+leogeek69@users.noreply.github.com>
2024-01-10 16:21:12 +0000

    Ajout de variable age et prenom Version 1.0.4

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 struct CompteBancaire {
     char nom[50];
+    char prenom[50];
+    int age;
     int numeroCompte;
     float solde;
 };
@@ -13,8 +15,14 @@ int main() {
     printf("Entrez votre nom : ");
     scanf("%s", nouveauCompte.nom);
 
+    printf("Entrez votre prénom : ");
+    scanf("%s", nouveauCompte.prenom);
+
     printf("Entrez le numéro de compte : ");
     scanf("%d", &nouveauCompte.numeroCompte);
+
+    printf("Entrez votre âge : ");
+    scanf("%d", &nouveauCompte.age);
 
     printf("Entrez le solde initial : ");
     scanf("%f", &nouveauCompte.solde);
@@ -22,6 +30,8 @@ int main() {
     // Informations du compte
     printf("\nCompte créé avec les informations suivante :\n");
     printf("Nom du titulaire : %s\n", nouveauCompte.nom);
+    printf("Prenom du titulaire : %s\n", nouveauCompte.prenom);
+    printf("Âge du titulaire : %d\n", nouveauCompte.age);
     printf("Numéro de compte : %d\n", nouveauCompte.numeroCompte);
     printf("Solde : %.2f\n", nouveauCompte.solde); // .2 pour deux chiffre après la virgule et "f" car c'est un float
 
